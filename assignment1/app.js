2d55a2464e59adb8c08fd726749d4eb1596025ef
Stijn Vermeeren <stijn@stijnvermeeren.be>
2016-12-26 14:36:38 +0100

    implement assignment

--- assignment1/app.js
+++ assignment1/app.js
@@ -1,6 +1,27 @@
 (function() {
 'use strict';
 
-angular.module('LunchCheck', []);
+angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.itemsInput = '';
+  $scope.message = '';
+  $scope.checkIfTooMuch = function() {
+    console.log(countItems($scope.itemsInput));
+    if ($scope.itemsInput == '') {
+      $scope.message = 'Please enter data first';
+    } else if (countItems($scope.itemsInput) <= 3) {
+      $scope.message = 'Enjoy!';
+    } else {
+      $scope.message = 'Too much!';
+    }
+  };
+}
+
+function countItems(inputString) {
+  return inputString.split(',').length;
+}
 
 })();
