88821f552e30d67fb7a1f181aae625763e2cb20f
Stijn Vermeeren <stijn@stijnvermeeren.be>
2016-12-27 22:18:37 +0100

    assignment 2 implementation

--- assignment2/app.js
+++ assignment2/app.js
@@ -9,28 +9,44 @@ angular.module('ShoppingListCheckOff', [])
 function ShoppingListCheckOffService() {
   var service = this;
 
-  var items;
+  var toBuy = [
+    { name: 'Achel', quantity: '6 bottles' },
+    { name: 'Westmalle', quantity: '24 bottles' },
+    { name: 'Westvleteren', quantity: '24 bottles' },
+    { name: 'Chimay', quantity: '6 bottles' },
+    { name: 'Rochefort', quantity: '6 bottles' },
+    { name: 'La Trappe', quantity: '6 bottles' }
+  ];
+
+  var alreadyBought = [];
+
+  service.getToBuy = function() {
+    return toBuy;
+  }
+
+  service.getAlreadyBought = function() {
+    return alreadyBought;
+  }
+
+  service.buy = function(itemIndex) {
+    alreadyBought.push(toBuy[itemIndex]);
+    toBuy.splice(itemIndex, 1);
+  }
 }
 
 ToBuyController.$inject = ['ShoppingListCheckOffService'];
 function ToBuyController(ShoppingListCheckOffService) {
   var toBuy = this;
 
-  toBuy.items = [
-    { name: 'Achel', quantity: 6 },
-    { name: 'Westmalle', quantity: 24 },
-    { name: 'Westvleteren', quantity: 24 },
-    { name: 'Chimay', quantity: 6 },
-    { name: 'Rochefort', quantity: 6 },
-    { name: 'La Trappe', quantity: 6 }
-  ];
+  toBuy.items = ShoppingListCheckOffService.getToBuy();
+  toBuy.buy = ShoppingListCheckOffService.buy;
 }
 
 ToBuyController.$inject = ['ShoppingListCheckOffService'];
 function AlreadyBoughtController(ShoppingListCheckOffService) {
   var alreadyBought = this;
 
-  alreadyBought.items = [];
+  alreadyBought.items = ShoppingListCheckOffService.getAlreadyBought();
 }
 
 
